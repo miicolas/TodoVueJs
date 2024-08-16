d22b8d55fc1bfd594a63a9e41c9e2608a59014f8
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-08-16 22:06:42 +0200

    add lunchPage

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../view/home.vue'
 import ViewTodo from '../view/viewTodo.vue' 
+import Start from '../view/start.vue'
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'home',
     component: Home,
   },
@@ -14,6 +15,11 @@ const routes = [
     name: 'todoDetail',
     component: ViewTodo,
   },
+  {
+    path: '/',
+    name: 'start',
+    component: Start,
+  }
 ]
 
 const router = createRouter({
---
+++ src/view/start.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const name = localStorage.getItem("name");
+
+const input_name = ref("");
+const save_name = () => {
+  if (input_name.value === "") {
+    alert("Please enter your name");
+    return;
+  }
+  localStorage.setItem("name", input_name.value);
+  router.push({ path: "/home" });
+};
+
+if (name) {
+  router.push({ path: "/home" });
+}
+</script>
+
+<template>
+  <div>NAME</div>
+  <input type="text" v-model="input_name" placeholder="Enter your name" />
+  <button @click="save_name">Save</button>
+</template>
