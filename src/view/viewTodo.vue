0b5a0f526a1ce1f3c9362726e79bdae427e8a409
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-08-16 22:07:42 +0200

    add info on single todo

--- src/view/viewTodo.vue
+++ src/view/viewTodo.vue
@@ -1,9 +1,40 @@
+<script setup>
+import { ref, watch, computed } from "vue";
+import { useRouter } from "vue-router";
+import Header from "../components/Header.vue";
+
+const router = useRouter();
+
+const id_todo = ref(router.currentRoute.value.params.id);
+
+const getLocalStorage = localStorage.getItem("todos");
+const todos = JSON.parse(getLocalStorage);
+const data_todo = ref({});
+
+
+todos.forEach((todo) => {
+  if (todo.id === id_todo.value) {
+    data_todo.value = todo;
+  }
+});
+</script>
+
 <template>
+  <Header />
+  <main>
+    <button @click="$router.go(-1)">Go back</button>
     <div>
       <h1>Todo Detail</h1>
-      <p>ID: {{ $route.params.id }}</p>
-    </div>
-  </template>
+      <p>ID: {{ id_todo }}</p>
+      <p>Content: {{ data_todo.content }}</p>
+      <p>Description: {{ data_todo.description }}</p>
+      <p>Done: {{ data_todo.done }}</p>
+      <p>Category: {{ data_todo.category }}</p>
 
-<script setup>
-</script>
+      
+
+    </div>
+  </main>
+</template>
+
+
